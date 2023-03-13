e4046c8a90db8f2409fa3875a8be8ce8839f8370
Maria <Maria@DESKTOP-H9PGOC5>
2023-03-13 17:40:24 +0100

    Добавьте файлы проекта.

---
+++ Naigebaver_IKM-620B_Course_Project/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Naigebaver_IKM_620B_Course_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bStart.Location = new System.Drawing.Point(235, 223);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(221, 108);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbinput
+            // 
+            this.tbinput.Enabled = false;
+            this.tbinput.Font = new System.Drawing.Font("Segoe UI Semibold", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbinput.Location = new System.Drawing.Point(262, 104);
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(174, 70);
+            this.tbinput.TabIndex = 2;
+            this.tbinput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbinput_KeyPress);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 383);
+            this.Controls.Add(this.tbinput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.Timer tClock;
+    }
+}
+
---
+++ Naigebaver_IKM-620B_Course_Project/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naigebaver_IKM_620B_Course_Project
+{
+    public partial class Form1 : Form
+    {
+        private bool Mode = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The time has passed!");
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+
+            if (Mode)
+            {
+                tbinput.Enabled = false;
+                Mode = false;
+                bStart.Text = "Пуск";
+                tClock.Stop();
+
+            }
+            else
+            {
+                tbinput.Enabled = true;
+                Mode = true;
+                bStart.Text = "Стоп";
+                tClock.Start(); 
+            }
+
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");// Виведення повідомлення
+tClock.Start();
+        }
+
+        private void tbinput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+               else
+            {
+                tClock.Start();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
+        }
+       
+    }
+}
